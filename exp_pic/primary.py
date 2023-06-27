fd24efa4ea786ce71951bc61a74f54cb40682084
JiangHai <hanayo@kayotin.cn>
2023-06-27 17:54:18 +0800

    update

--- exp_pic/primary.py
+++ exp_pic/primary.py
@@ -30,17 +30,19 @@ for data in datas:
 # for image in images_list:
 #     headers["referer"] = image["referer"]
 #     print(headers)
+# 获取第一张图片
 image_1 = images_list[0]
-# headers["referer"] = image_1["referer"]
-# print(headers)
+# 通过以下链接，请求图片详情
 image_url = f"https://www.pixiv.net/ajax/illust/{image_1['p_id']}/pages?lang=zh"
 resp_image = requests.get(image_url, headers=headers)
+# 数据保存在body字段
 image_data = resp_image.json()["body"]
-# for page in image_data:
-#     print(page["urls"]["original"])
+# 因为图片可能有多p，所以是一个列表。我们拿第一个的urls字段，其中的original就是原图
 download_url = image_data[0]["urls"]["original"]
 download_headers = headers
+# 如果不加referer字段，直接请求下载链接p站不给结果
 download_headers["referer"] = image_1["referer"]
+# 通过如下请求，我们获得了最终的下载链接
 resp_final = requests.get(download_url, headers=download_headers)
 file_name = download_url.split(".")[-1]
 with open(f"output/{image_1['p_id']}.{file_name}", "wb") as file:
